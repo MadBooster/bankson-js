1a05d02cb621c05be2515ab78d20c032011a2bba
Tomi Nokkala <tol1@iki.fi>
2025-05-28 10:50:20 +0300

    Use eslint 9

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'eslint/config'
+import eslintConfigMadboosterNodeApp from 'eslint-config-madbooster-node-app'
+
+export default defineConfig([
+  {
+    ignores: ['lib'],
+  },
+  {
+    files: ['src/**/*.{ts,js}'],
+    extends: eslintConfigMadboosterNodeApp,
+  },
+])
