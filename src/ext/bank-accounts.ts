3739b8f6011391c3054491041ebeb245e166cec0
Tomi Nokkala <tol1@iki.fi>
2026-02-02 11:26:24 +0200

    Further improve bank account api type

--- src/ext/bank-accounts.ts
+++ src/ext/bank-accounts.ts
@@ -3,9 +3,11 @@ import Qs from 'qs'
 import BaseSubClient from '../baseSubClient.js'
 import type { BankAccountResponse, BaseResponse } from '../types.js'
 
-type BankAccountData = Omit<BankAccountResponse, 'id' | 'customer_information'> & {
+export type BankAccountCreateData = Omit<BankAccountResponse, 'id' | 'customer_information'> & {
   customer_information: Pick<NonNullable<BankAccountResponse['customer_information']>, 'name' | 'business_id'>
 }
+
+export type BankAccountUpdateData = Pick<BankAccountCreateData, 'contract_id'> & { customer_information?: Partial<BankAccountCreateData['customer_information']> | null }
 
 interface BankAccountFilters {
   offset?: number
@@ -17,11 +19,11 @@ export default class BankAccounts extends BaseSubClient {
     return this.base.get<BaseResponse<BankAccountResponse>>('/v2/bank-accounts?' + Qs.stringify(opts))
   }
 
-  createV2(data: BankAccountData) {
-    return this.base.post<BankAccountData, BankAccountResponse>('/v2/bank-accounts', data)
+  createV2(data: BankAccountCreateData) {
+    return this.base.post<BankAccountCreateData, BankAccountResponse>('/v2/bank-accounts', data)
   }
 
-  updateV2(id: string, data: BankAccountData) {
-    return this.base.put<Pick<BankAccountData, 'contract_id' | 'customer_information'>, BankAccountResponse>(`/v2/bank-accounts/${id}`, data)
+  updateV2(id: string, data: BankAccountUpdateData) {
+    return this.base.put<BankAccountUpdateData, BankAccountResponse>(`/v2/bank-accounts/${id}`, data)
   }
 }
